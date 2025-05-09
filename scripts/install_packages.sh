56d74ef4f2adb6980b483f29ce532f389ca32bbc
Damoa <damoa.obiri.alcorta@sofatutor.com>
2025-05-09 08:27:11 +0200

    Add zoxide to install packages.

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -1,1 +1,8 @@
 sudo apt install curl direnv jq
+
+if command -v python >/dev/null 2>&1; then
+  echo "zoxide is already installed"
+else
+  sudo apt install zoxide
+  echo "eval \"\$(zoxide init bash)\"" >> ~/bashrc/.bashrc
+fi
