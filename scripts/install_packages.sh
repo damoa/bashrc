2b7250e1a30aa348e0a42c195f17dfe26582657f
Damoa <damoa.obiri.alcorta@sofatutor.com>
2025-05-16 10:54:04 +0200

    Install ripgrep

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -7,5 +7,16 @@ else
   echo "eval \"\$(zoxide init bash)\"" >> ~/bashrc/.bashrc
 fi
 
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
+if command -v fzf >/dev/null 2>&1; then
+  echo "fzf is already installed"
+else
+  git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+  ~/.fzf/install
+fi
+
+if command -v rg >/dev/null 2>&1; then
+  echo "ripgrep is already installed"
+else
+  curl -LO https://github.com/BurntSushi/ripgrep/releases/download/14.1.0/ripgrep_14.1.0-1_amd64.deb
+  sudo dpkg -i ripgrep_14.1.0-1_amd64.deb
+fi
