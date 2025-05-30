05666a79f96ae9e8df8a286c5cea33e011a96e70
Damoa <damoa.obiri.alcorta@sofatutor.com>
2025-05-30 14:31:27 +0200

    Add specialized ollama models in comments as suggestions.

--- scripts/run_local_llm.sh
+++ scripts/run_local_llm.sh
@@ -12,6 +12,9 @@ ollama serve
 # # phi-3 requires 1.5GB
 # # codellama:7b requires 8GB+ of RAM
 # # there is also codellama:34b  that requires 32GB+ RAM
+# # codellama:7b-instruct for natural language + code
+# # codellama:7b-code for code
+# # codellama:7b-python for code
 # if command -v ollama >/dev/null 2>&1; then
 #   if ollama list | grep -q '^phi-3'; then
 #     echo 'phi3 is already pulled'
