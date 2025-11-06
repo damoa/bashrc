90703df9153d1a5f21b157eea84fd3192b75039c
d4m04 <damoa@posteo.de>
2025-11-06 09:37:39 +0100

    Update general install packages script.

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -22,8 +22,18 @@ else
   if [ "$arch" = "armv7l" ]; then
     curl -LO https://github.com/BurntSushi/ripgrep/releases/download/14.1.0/ripgrep_14.1.0-1_armv7.deb
     sudo dpkg -i ripgrep_14.1.0-1_armv7.deb
+  elif [ "$arch" = "aarch64" ]; then
+    sudo apt install ripgrep
+    # wget http://ftp.de.debian.org/debian/pool/main/r/rust-ripgrep/ripgrep_15.1.0-1+b4_arm64.deb
+    # sudo dpkg -i ripgrep_14.1.1-1+b4_arm64.deb
   else
     curl -LO https://github.com/BurntSushi/ripgrep/releases/download/14.1.0/ripgrep_14.1.0-1_amd64.deb
     sudo dpkg -i ripgrep_14.1.0-1_amd64.deb
   fi
+fi
+
+if command -v newsboat >/dev/null 2>&1; then
+  echo "newsboat is already installed"
+else
+  sudo apt install newsboat
 fi
